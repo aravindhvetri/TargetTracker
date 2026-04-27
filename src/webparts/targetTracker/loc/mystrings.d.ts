c27d843892c6ffd3e19503b02ca0034112b12a3e
aravindhvetri <2002aravinthv@gmail.com>
2026-04-27 12:28:22 +0530

    first initial push to main

---
+++ src/webparts/targetTracker/loc/mystrings.d.ts
@@ -0,0 +1,19 @@
+declare interface ITargetTrackerWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+  UnknownEnvironment: string;
+}
+
+declare module 'TargetTrackerWebPartStrings' {
+  const strings: ITargetTrackerWebPartStrings;
+  export = strings;
+}
